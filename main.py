58439a2510c88be52e2b37b8404f8df83459bbf8
Владислав Курочкин <vlad555.95@mail.ru>
2018-01-08 21:00:01 +0400

    fixed params

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import make_models
 import test_classify
 import test_recognize
 
-train_params = {'from': 0, 'to': 5}
-test_params = {'from': 5, 'to': 20}
+train_params = {'from': 0, 'to': 10}
+test_params = {'from': 10, 'to': 20}
 
 make_models.start(train_params)
 test_classify.start(test_params)
